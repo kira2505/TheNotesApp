f23dedab9cd44d42772602769af394f87268937f
Kira <kirass0525@gmail.com>
2025-11-18 13:24:02 +0100

    Create and update
    Create entity Note, enum Tag and repository;
    Update pom.xml

---
+++ src/main/java/com/notesapp/entity/Note.java
@@ -0,0 +1,35 @@
+package com.notesapp.entity;
+
+import com.notesapp.enums.Tag;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "notes")
+public class Note {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String text;
+
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    private Set<Tag> tags;
+}
